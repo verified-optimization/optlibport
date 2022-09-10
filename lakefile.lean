b767ddfc86555b4f32f881f54575d4672cdb1c87
Alexander Bentkamp <bentkamp@gmail.com>
2022-09-10 10:34:11 +0200

    copy from mathlib3port and adapt

---
+++ lakefile.lean
@@ -0,0 +1,46 @@
+import Lake
+open Lake DSL System
+
+-- Usually the `tag` will be of the form `nightly-2021-11-22`.
+-- If you would like to use an artifact from a PR build,
+-- it will be of the form `pr-branchname-sha`.
+def tag : String := "pr-optlib-3ee2c81"
+def releaseRepo : String := "abentkamp/mathport"
+def oleanTarName : String := "optlib-binport.tar.gz"
+
+def download (url : String) (to : FilePath) : BuildM PUnit := Lake.proc
+{ -- We use `curl -O` to ensure we clobber any existing file.
+  cmd := "curl",
+  args := #["-L", "-O", url]
+  cwd := to }
+
+def untar (file : FilePath) : BuildM PUnit := Lake.proc
+{ cmd := "tar",
+  args := #["-xzf", file.fileName.getD "."] -- really should throw an error if `file.fileName = none`
+  cwd := file.parent }
+
+def getReleaseArtifact (repo tag artifact : String) (to : FilePath) : BuildM PUnit :=
+download s!"https://github.com/{repo}/releases/download/{tag}/{artifact}" to
+
+def untarReleaseArtifact (repo tag artifact : String) (to : FilePath) : BuildM PUnit := do
+  getReleaseArtifact repo tag artifact to
+  untar (to / artifact)
+
+package optlibport where
+  extraDepTargets := #[`fetchOleans]
+
+target fetchOleans (_pkg : Package) : Unit := do
+  let libDir : FilePath := __dir__ / "build" / "lib"
+  IO.FS.createDirAll libDir
+  let oldTrace := Hash.ofString tag
+  let _ ← buildFileUnlessUpToDate (libDir / oleanTarName) oldTrace do
+    logInfo "Fetching oleans for Mathbin"
+    untarReleaseArtifact releaseRepo tag oleanTarName libDir
+  return .nil
+
+require mathlib3port from git "https://github.com/leanprover-community/mathlib3port.git"@"5f609e38274cee3b230eba16e2c543f6e389fd76"
+
+@[defaultTarget]
+lean_lib Optbin where
+  roots := #[]
+  globs := #[`Optbin]
